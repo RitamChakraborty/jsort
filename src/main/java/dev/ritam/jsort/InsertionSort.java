e97764c6cecfc39aa3988160abb276ee10bbeb99
Ritam Chakraborty <mailbox.ritamchakraborty@gmail.com>
2022-07-23 23:05:47 +0530

    Add insertion sort algorithm

--- src/main/java/dev/ritam/jsort/InsertionSort.java
+++ src/main/java/dev/ritam/jsort/InsertionSort.java
@@ -7,6 +7,16 @@ class InsertionSort<T extends Comparable<T>> extends SortingUtil<T> implements S
 
     @Override
     public void sort() {
-        throw new UnsupportedOperationException();
+        for (int i = 1; i < getLength(); ++i) {
+            T temp = array[i];
+            int j = i - 1;
+
+            while (j >= 0 && array[j].compareTo(temp) > 0) {
+                array[j + 1] = array[j];
+                --j;
+            }
+
+            array[j + 1] = temp;
+        }
     }
 }
