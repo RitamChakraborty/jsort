423c227a2dc0f2fd85c5b4d7b0e96377e74b1ca1
Ritam Chakraborty <mailbox.ritamchakraborty@gmail.com>
2022-07-23 23:25:44 +0530

    Add selection sort algorithm

--- src/main/java/dev/ritam/jsort/SelectionSort.java
+++ src/main/java/dev/ritam/jsort/SelectionSort.java
@@ -7,6 +7,18 @@ class SelectionSort<T extends Comparable<T>> extends SortingUtil<T> implements S
 
     @Override
     public void sort() {
-        throw new UnsupportedOperationException();
+        for (int i = 0; i < getLength(); ++i) {
+            T temp = array[i];
+            int swapIndex = i;
+
+            for (int j = i + 1; j < getLength(); ++j) {
+                if (temp.compareTo(array[j]) > 0) {
+                    swapIndex = j;
+                    temp = array[j];
+                }
+            }
+
+            swap(i, swapIndex);
+        }
     }
 }
